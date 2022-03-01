6c4fa0da9f25e6d226b00cb12a207bf249b3b3d4
FidelisMomanyi <fidelismomanyi001@gmail.com>
2022-03-01 09:12:30 +0300

    edited views.py

--- cropper/views.py
+++ cropper/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render, redirect
+# from .models import Photo
+# from .forms import PhotoForm
 from django.shortcuts import render
+from django.http import Http404
+from .models import Location, Category, Image
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
