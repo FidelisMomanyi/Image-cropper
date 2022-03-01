a059b3282780d386b37b8e9a9f1ff92933033c00
FidelisMomanyi <fidelismomanyi001@gmail.com>
2022-03-01 09:49:26 +0300

    installed bootstrap3

--- imagecropper/settings.py
+++ imagecropper/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cropper',
     'bootstrap3',
-    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
