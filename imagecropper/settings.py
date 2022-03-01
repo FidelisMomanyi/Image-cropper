2e9c27f3f1b30a264dc19e3dcfbf9e7036fd226d
FidelisMomanyi <fidelismomanyi001@gmail.com>
2022-03-01 09:36:33 +0300

    installed apps

--- imagecropper/settings.py
+++ imagecropper/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cropper',
+    'bootstrap3',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
