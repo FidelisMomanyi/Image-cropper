957a5e4ead62fcf55bb8094e2f837c139e0b944e
FidelisMomanyi <fidelismomanyi001@gmail.com>
2022-03-02 08:35:04 +0300

    cropper directory (tests.py)

--- cropper/tests.py
+++ cropper/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Category, Location, Image
+import datetime as dt
+
+
+class LocationTestCase(TestCase):
+    def setUp(self):
+        Location.objects.create(name='nakuru')
+        self.name = Location(name='nakuru')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name, Location))
+
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='facebook')
+        self.name = Category(name='facebook')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name, Category))
+
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Nakuru')
+
+        self.location.save()
+
+        self.category = Category(name='personal')
+        self.category.save()
+
+        self.image_test = Image(id=1, image='image.jpg', description='test image', location=self.location,
+                                category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id, 'photos/test.jpg')
+        changed_img = Image.objects.filter(image='photos/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
